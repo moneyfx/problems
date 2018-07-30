822eee80f197f0a04dfa003bc31fd18658f17ae7
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-07-30 00:25:23 -0400

    move to 001

--- 001_reverse_string/solution.js
+++
@@ -1,6 +0,0 @@
-//https://leetcode.com/problems/reverse-string/description/
-const reverseString = function (str) {
-    return str.split('').reverse().join('');
-};
-
-console.log(reverseString('hello')); // olleh
