2279006a1ba749e1aedb2671ed5a90d2c55d2110
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-08-18 18:46:37 -0400

    reverse string without built-in methods

--- 0001_reverse_string/Solution.java
+++ 0001_reverse_string/Solution.java
@@ -1,11 +1,26 @@
 public class Solution {
-
     public String reverseString(String s) {
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+            i++;
+            j--;
+        }
+        
+        return new String(arr);
+    }
+
+    public String reverseStringByStringBuilder(String s) {
         return new StringBuilder(s).reverse().toString();
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(s.reverseString("hello")); // olleh
+        System.out.println(s.reverseStringByStringBuilder("hello")); // olleh
     }
 }
