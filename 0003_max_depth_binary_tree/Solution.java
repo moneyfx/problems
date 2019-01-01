5b3127523a31b38d98fc4a206324796f6573e276
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-12-31 20:41:22 -0500

    max of binary tree

---
+++ 0003_max_depth_binary_tree/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftMax = root.left == null ? 0 : maxDepth(root.left);
+        int rightMax = root.right == null ? 0 : maxDepth(root.right);
+
+        return 1 + Math.max(leftMax, rightMax);
+    }
+}
